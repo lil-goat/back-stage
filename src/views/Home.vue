d5137041670c946344d5f177f7f4bf360c31fef3
goat <1211928420@qq.com>
2025-10-29 16:39:23 +0800

    完成侧边栏和部分home组件

---
+++ src/views/Home.vue
@@ -0,0 +1,149 @@
+<script setup>
+import { ref } from 'vue'
+const getImg = (user) => new URL(`../assets/images/${user}.png`,import.meta.url).href
+//这个tableData是假数据，等会我们使用axios请求mock数据
+const tableData = ref([
+    {
+      name: "Java",
+      todayBuy: 100,
+      monthBuy: 200,
+      totalBuy: 300,
+    },
+    {
+      name: "Python",
+      todayBuy: 100,
+      monthBuy: 200,
+      totalBuy: 300,
+    }
+])
+
+const tableLabel = ref({
+    name: "课程",
+    todayBuy: "今日购买",
+    monthBuy: "本月购买",
+    totalBuy: "总购买",
+})
+
+</script>
+
+<template>
+  <el-row class="home" :gutter="20">
+    <el-col :span="8" style="margin-top: 20px;">
+      <el-card show="hover">
+        <div class="user">
+          <img :src="getImg('user')" alt="">
+          <div class="user-info">
+            <p class="user-info-admin">Admin</p>
+            <p class="user-info-p">超级管理员</p>
+          </div>
+        </div>
+        <div class="login-info">
+          <p>上次登录时间:<span>2024-06-30</span></p>
+          <p >上次登录地点:<span>北京</span></p>
+        </div>
+      </el-card>
+
+      <el-card show="hover" class="user-table">
+        <el-table :data="tableData">
+          <el-table-column
+            v-for="(val,key) in tableLabel"
+            :key="key"
+            :prop="key"
+            :label="val"
+          >
+
+          </el-table-column>
+        </el-table>
+      </el-card>
+    </el-col>
+    <el-col :span="12"></el-col>
+  </el-row>
+</template>
+
+
+<style scoped lang="less">
+  .home{
+   height:100%;
+   overflow:hidden;
+   .user{
+      display:flex;
+      align-items:center;
+      border-bottom:1px solid #ccc;
+      margin-bottom:25px;
+      padding-bottom: 25px; 
+      img{
+         width:150px;
+         height:150px;
+         border-radius:50%;
+         margin-right:40px; 
+
+      }
+      .user-info{
+         p{
+            line-height:40px;
+         }
+         .user-info-p{
+            color:#999;
+         }
+         .user-info-admin{
+            font-size:35px
+         }
+      }
+   }
+   .login-info{
+      p{
+         line-height:30px;
+         font-size:14px;
+         color:#999;
+         span{
+            color:#666;
+            margin-left:60px;
+         }
+      }
+   }
+   .user-table{
+      margin-top:20px;
+   }
+   .num{
+      display:flex;
+      flex-wrap:wrap;
+      justify-content:space-between;
+      .el-card{
+         width:32%;
+         margin-bottom:20px;
+      }
+      .icons{
+         width:80px;
+         height:80px;
+         font-size:30px;
+         text-align:center;
+         line-height:80px;
+         color:#fff;
+      }
+      .detail{
+         margin-left:15px;
+         display:flex;
+         flex-direction:column;
+         justify-content:center;
+         .num{
+            font-size:30px;
+            margin-bottom:10px
+         }
+         .txt{
+            font-size:15px;
+            text-align:center;
+            color:#999;
+         }
+      }
+   }
+   .graph{
+      margin-top:20px;
+      display:flex;
+      justify-content:space-between;
+      .el-card{
+         width:48%;
+         height:260px;
+      }
+   }
+  }
+</style>
